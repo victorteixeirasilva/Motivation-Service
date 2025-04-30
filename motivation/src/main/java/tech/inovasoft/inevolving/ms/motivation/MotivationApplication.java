c063502ad4a59121ff95e14b530f3bffd1928828
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-30 20:51:00 -0300

    Feat: Commit Inicial

---
+++ motivation/src/main/java/tech/inovasoft/inevolving/ms/motivation/MotivationApplication.java
@@ -0,0 +1,13 @@
+package tech.inovasoft.inevolving.ms.motivation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivationApplication.class, args);
+	}
+
+}
