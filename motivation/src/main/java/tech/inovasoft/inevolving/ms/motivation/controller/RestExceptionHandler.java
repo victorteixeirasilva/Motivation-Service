4207f90c619198ba36ce80f8858642d0a47f8fb9
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-05-03 13:36:24 -0300

    feat: finalizando tratamento de exceções

--- motivation/src/main/java/tech/inovasoft/inevolving/ms/motivation/controller/RestExceptionHandler.java
+++ motivation/src/main/java/tech/inovasoft/inevolving/ms/motivation/controller/RestExceptionHandler.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import tech.inovasoft.inevolving.ms.motivation.domain.dto.response.ExceptionResponse;
-import tech.inovasoft.inevolving.ms.motivation.domain.exception.MaximumNumberOfRegisteredDreamsException;
+import tech.inovasoft.inevolving.ms.motivation.domain.exception.*;
 
-//@ControllerAdvice(basePackages = {"motivation.src.main.java.tech.inovasoft.inevolving.ms.motivation.controller"})
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -17,8 +16,51 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.FORBIDDEN)
                 .body(new ExceptionResponse(
-                        exception.getClass().getName(),
+                        exception.getClass().getSimpleName(),
                         exception.getMessage()));
+    }
+
+    @ExceptionHandler(NotSavedDTOInDbException.class)
+    private ResponseEntity notSavedDTOInDbException(NotSavedDTOInDbException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ExceptionResponse(
+                        exception.getClass().getSimpleName(),
+                        exception.getMessage()
+                ));
+    }
+
+
+    @ExceptionHandler(UserWithoutAuthorizationAboutThisDreamException.class)
+    private ResponseEntity userWithoutAuthorizationAboutThisDreamException(UserWithoutAuthorizationAboutThisDreamException exception) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(new ExceptionResponse(
+                        exception.getClass().getSimpleName(),
+                        exception.getMessage()
+                ));
+    }
+
+
+    @ExceptionHandler(DreamNotFoundException.class)
+    private ResponseEntity dreamNotFoundException(DreamNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ExceptionResponse(
+                        exception.getClass().getSimpleName(),
+                        exception.getMessage()
+                ));
+    }
+
+
+    @ExceptionHandler(DataBaseException.class)
+    private ResponseEntity dataBaseException(DataBaseException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ExceptionResponse(
+                        exception.getClass().getSimpleName(),
+                        exception.getMessage()
+                ));
     }
 
 
