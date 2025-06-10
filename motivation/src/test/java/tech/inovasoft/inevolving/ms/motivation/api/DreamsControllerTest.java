7e1207bb767807c546bb26567983daf6c4c03155
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-10 16:37:38 -0300

    Desenvolvendo teste do End-point, updateDream_ok

--- motivation/src/test/java/tech/inovasoft/inevolving/ms/motivation/api/DreamsControllerTest.java
+++ motivation/src/test/java/tech/inovasoft/inevolving/ms/motivation/api/DreamsControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tech.inovasoft.inevolving.ms.motivation.domain.dto.request.DreamRequestDTO;
+import tech.inovasoft.inevolving.ms.motivation.domain.model.Dreams;
 
 import java.util.UUID;
 
@@ -51,9 +52,61 @@ public class DreamsControllerTest {
                 .body("idUser", equalTo(idUser.toString()));
     }
 
+    private UUID addDream(){
+        DreamRequestDTO request = new DreamRequestDTO(
+                "Nome do Sonho",
+                "Descrição do Sonho",
+                "https://inovasoft.tech/wp-content/webp-express/webp-images/uploads/2025/03/LogoTipoBorda-512x319px.png.webp",
+                idUser
+        );
+
+        RequestSpecification requestSpecification = given()
+                .contentType(ContentType.JSON);
+
+        ValidatableResponse response = requestSpecification
+                .body(request)
+                .when()
+                .post("http://localhost:" + port + "/ms/motivation/dreams")
+                .then();
+
+        response.assertThat().statusCode(200).and()
+                .body("id", notNullValue()).and()
+                .body("name", equalTo(request.name())).and()
+                .body("description", equalTo(request.description())).and()
+                .body("urlImage", equalTo(request.urlImage())).and()
+                .body("idUser", equalTo(idUser.toString()));
+
+        return UUID.fromString(response.extract().body().jsonPath().get("id"));
+    }
+
     @Test
     public void updateDream_ok() {
-        //TODO: Desenvolver teste do End-Point
+
+        UUID idDream = addDream();
+
+        var request = new Dreams(
+                idDream,
+                "Novo Nome do Sonho",
+                "Novo Descrição do Sonho",
+                "https://inovasoft.tech/wp-content/webp-express/webp-images/uploads/2025/03/LogoTipoBorda-512x319px.png.webp",
+                idUser
+        );
+
+        RequestSpecification requestSpecification = given()
+                .contentType(ContentType.JSON);
+
+        ValidatableResponse response = requestSpecification
+                .body(request)
+                .when()
+                .patch("http://localhost:" + port + "/ms/motivation/dreams")
+                .then();
+
+        response.assertThat().statusCode(200).and()
+                .body("id", equalTo(request.getId().toString())).and()
+                .body("name", equalTo(request.getName())).and()
+                .body("description", equalTo(request.getDescription())).and()
+                .body("urlImage", equalTo(request.getUrlImage())).and()
+                .body("idUser", equalTo(idUser.toString()));
     }
 
     @Test
